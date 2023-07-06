769f3f958c931faf4da582b867a27c3c1db0ec5d
nico858 <nicolasurbanos10@gmail.com>
2023-07-06 17:08:07 -0500

    ,

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'https://localhost:5173',
+    origin: 'https://nico858.github.io',
     credentials: true,
 }));
 routerAPi(app);
